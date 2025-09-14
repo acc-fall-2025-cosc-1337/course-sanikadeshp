cccdf397c720198fd909ac2a993fcc193747a32b
Sanika Deshpande <sanikadeshp@gmail.com>
2025-09-14 04:05:50 +0000

    echo_variable w unit test case

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,14 @@
 ﻿//write include statements
-
+#include "data_types.h" 
+#include <iostream>
 
 //write namespace using statement for cout
-
+using namespace std;
 
 int main()
 {
-	
+    cout << "7 x 7 = " << multiply_numbers(7, 7) << endl;
+    cout << "5 x 5 = " << multiply_numbers(5, 5) << endl;
 
-	return 0;
+    return 0;
 }
